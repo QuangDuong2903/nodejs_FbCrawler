4463511f502e16dcd9257aaf1a9ccfa3cf34237d
quangduong <quangduongptsc@gmail.com>
2022-07-02 12:20:25 +0700

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const puppeteer = require('puppeteer')
-const port = 3000
+const port = process.env.PORT
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
